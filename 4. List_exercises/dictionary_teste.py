b1ddeba78092799712c03703e03ca66b2dbc9ace
gkbarros <gb397@hotmail.com>
2022-09-03 11:49:39 -0300

    New Exercises

---
+++ 4. List_exercises/dictionary_teste.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média maior ou igual a 7.0.
+name = str(input("Type your name here:"))
+grade = [float(input("Type your grade here:"))]
+data_students = {
+    name : grade
+}
+print(data_students)
+
---
+++ 4. List_exercises/dictionary_teste02.py
@@ -0,0 +1,9 @@
+#teste pra ver se dicionario recebe uma lista como valor de uma chave 
+teste = "Gustavo"
+lista = [
+    1, 2, 3
+    ]
+dic = {
+    teste : lista
+}
+print(dic)
