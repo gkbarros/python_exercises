5c64921182cd96e7192d701ca837c32610b6725b
gkbarros <gb397@hotmail.com>
2022-09-12 08:41:57 -0300

    Add Exercise

--- 5. Exercicios Funções/fun06.py
+++ 5. Exercicios Funções/fun06.py
@@ -4,7 +4,31 @@
 # Neste momento o programa deverá ser encerrado, exibindo o relatório do dia, que conterá a quantidade e o valor total de prestações pagas no dia. O cálculo do valor a ser pago é feito da seguinte forma.
 # Para pagamentos sem atraso, cobrar o valor da prestação.
 # Quando houver atraso, cobrar 3% de multa, mais 0,1% de juros por dia de atraso.
-valor_prestação = float(input("Type your boleto here:"))
-dias_atraso = int(input('type the days late here:'))
-def valor_pagamento (a,b):
-    return
+def pay(vl,dl):
+    if dl < 1:
+        print(vl)
+        return vl
+    else:
+        vl = vl + (vl * 3/100)+((vl * 0.1/100)*dl)
+        print(vl)
+        return(vl)
+
+vl = 0
+dl = 0
+pay_list = []
+pay_check = 0
+pay_total = 0
+
+while True:
+    print('Type 0 to exit')
+    vl = int(input('Type here the value :'))
+    if vl == 0:
+        break
+    dl = int(input('Type here days late :'))
+    pay_list.append(pay(vl,dl))
+    pay_check += 1
+    pay_total += pay(vl,dl)
+
+print('A total of '+str(len(pay_list))+' paid debts. With a total value of '+str(pay_total))
+
+print(pay_list)
